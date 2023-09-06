2e7565a63549d1e4638f5ce756bc7563bd8fe7a4
XanderBurger <86130720+XanderBurger@users.noreply.github.com>
2023-09-06 10:54:56 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import P5app from './sketch/sketch';
+import logo from './images/ACRM-white.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='outer'>
+      <h1 className='header'>Come See the History of Progress </h1>
+      <div className='sketch'>
+        <P5app/>
+      </div>
+      <div className='spacer'></div>
+      <h1 className='lowerHeader'>
+        @ the AMRC
+        <a href="https://acrmuseum.org/" className='link'>Plan your visit</a>
+      </h1>
+      <image src={logo} alt="logo" className='logo'/>
+    </div>   
   );
 }
 
---
+++ src/sketch/sketch.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import {ReactP5Wrapper} from '@p5-wrapper/react'
+
+function sketch(p5) {
+
+    p5.setup = () => {
+        p5.createCanvas(p5.windowWidth, p5.windowHeight)
+    };
+
+    p5.draw = () => {
+        p5.background(0);
+        p5.noFill();
+        p5.stroke("#894F9F");
+        p5.strokeWeight(10);
+        p5.strokeCap(p5.SQUARE);
+        p5.bezier(50, 400, p5.mouseX, p5.mouseY, p5.mouseX,-p5.mouseY+600, p5.windowWidth - 50, 400);
+        p5.stroke("#EC5824");
+        p5.bezier(50, 350, p5.mouseX, p5.mouseY-25, p5.mouseX,-(p5.mouseY * 1.1)+600, p5.windowWidth - 50, 350);
+        p5.stroke("#FBAA27");
+        p5.bezier(50, 300, p5.mouseX, p5.mouseY-50, p5.mouseX, -(p5.mouseY * 1.15)+600, p5.windowWidth - 50, 300);
+        p5.stroke("#EE1C4E")
+        p5.bezier(50, 250, p5.mouseX, p5.mouseY-75, p5.mouseX, -(p5.mouseY * 1.2)+600, p5.windowWidth - 50, 250);
+    };
+        
+
+}
+
+
+export default function P5app() {
+    return <ReactP5Wrapper sketch={sketch}/>
+}
