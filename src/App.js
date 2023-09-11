87aa2e8d46b3345cd7af92de7fe0c781dd840304
XanderBurger <86130720+XanderBurger@users.noreply.github.com>
2023-09-11 09:57:55 -0600

    added sketch

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import logo from './images/ACRM-white.png'
 function App() {
   return (
     <div className='outer'>
-      <h1 className='header'>Come See the History of Progress </h1>
+      <h1 className='header'>THE HISTORY OF</h1>
       <div className='sketch'>
         <P5app/>
       </div>
       <div className='spacer'></div>
       <h1 className='lowerHeader'>
-        @ the ACRM
+        @ the American Computer <br/>
+        & Robotics Museum
         <a href="https://acrmuseum.org/" className='link'>Plan your visit</a>
       </h1>
       <image src={logo} alt="logo" className='logo'/>
--- src/sketch/sketch.jsx
+++ src/sketch/sketch.jsx
@@ -3,34 +3,74 @@ import {ReactP5Wrapper} from '@p5-wrapper/react'
 import Satoshi from '../fonts/Satoshi-Variable.ttf'
 
 function sketch(p5) {
-    
+
+    let satoshi
+    let textPoints
+    let canvasWidth = p5.windowWidth
+    let canvasHeight = p5.windowHeight
+    let title = "Computer"
+    let colors = ["#FFFFFF", "#F22F66", "#FC5F1A", "#FFE027", "#80D860", "#3263ED"]
+    let words = ["COMPUTERS", "ROBOTS", "MACS", "ENIGMA", "QUANTUM", "AI", "PROGRESS"]
+    let colorNum = 0
+    let textSize = 200
+
+    function drawLetter(letter,x,y, colorNum, sampleFactor, squareSize) {
+        textPoints = satoshi.textToPoints(letter, x, y, textSize, {sampleFactor: sampleFactor})
+        p5.fill("#FFFFF")
+        p5.noStroke()
+        p5.fill(colors[colorNum])
+        p5.beginShape()
+        for (let i = 0; i < textPoints.length; i++) {
+            let p = textPoints[i];
+            p5.vertex((Math.round(p.x / squareSize.x) * squareSize.x),Math.round(p.y/squareSize.y) * squareSize.y)
+        }
+        p5.endShape(p5.ClOSE)
+    }
+
     p5.preload = () => {
-        const satoshi = p5.loadFont(Satoshi)
+        satoshi = p5.loadFont(Satoshi)
     }
 
     p5.setup = () => {
-        p5.createCanvas(p5.windowWidth, p5.windowHeight)
-        p5.filter(p5.BLUR, 3)
+        p5.createCanvas(canvasWidth, canvasHeight)
+        p5.frameRate(30)
     };
-
+    
+    let sizeX = 0
+    let sizeY = 0
+    let count = 0
+    let w = 0
+    let y = 450
     p5.draw = () => {
-        p5.background(0);
-        p5.noFill();
-        p5.stroke("#894F9F");
-        p5.strokeWeight(10);
-        p5.strokeCap(p5.SQUARE);
-        p5.bezier(50, 400, p5.mouseX, p5.mouseY, p5.mouseX,-p5.mouseY+600, p5.windowWidth - 50, 400);
-        p5.stroke("#EC5824");
-        p5.bezier(50, 350, p5.mouseX, p5.mouseY-25, p5.mouseX,-(p5.mouseY * 1.1)+600, p5.windowWidth - 50, 350);
-        p5.stroke("#FBAA27");
-        p5.bezier(50, 300, p5.mouseX, p5.mouseY-50, p5.mouseX, -(p5.mouseY * 1.15)+600, p5.windowWidth - 50, 300);
-        p5.stroke("#EE1C4E")
-        p5.bezier(50, 250, p5.mouseX, p5.mouseY-75, p5.mouseX, -(p5.mouseY * 1.2)+600, p5.windowWidth - 50, 250);
-    };
-        
+        p5.background("#000000");
+        p5.textFont(satoshi)
+        p5.textSize(textSize)
+        count++
+        sizeX += Math.cos(count/30) 
+        sizeY += Math.sin(count/30) 
+        if (Math.cos(count/30) < -0.999){
+            w = (w + 1) % words.length
+        }
+        title = words[w]
 
+        let squareSize = {x: sizeX, y: sizeY}
+        let x = 52
+        for (let i = 0; i < title.length; i++) {
+            drawLetter(title[i],x, y, 3, 0.2, squareSize)
+            x += p5.textWidth(title[i])
+        }
+        x = 48
+        for (let i = 0; i < title.length; i++) {
+            drawLetter(title[i],x, y, 1, 0.2, squareSize)
+            x += p5.textWidth(title[i])
+        }
+        x= 50
+        for (let i = 0; i < title.length; i++) {
+            drawLetter(title[i],x, y, 0, 0.5, squareSize)
+            x += p5.textWidth(title[i])
+        }
+    }
 }
-
 
 export default function P5app() {
     return <ReactP5Wrapper sketch={sketch}/>
